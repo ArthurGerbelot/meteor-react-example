714d0e529455419a4ebcfdd60a8ab970ef3d0b97
Arthur G <arthur.gerbelot@gmail.com>
2015-12-21 09:56:01 -0500

    Fix reactivity on relation between games and users

--- lib/containers/game.jsx
+++ lib/containers/game.jsx
@@ -6,12 +6,22 @@ GameApp = React.createClass({
     if (handle_singleGame.ready()) {
       console.log("--data.game reload--");
       data.game = Games.findOne({_id: this.props.game_id});
+
+      console.log(data.game);
+      var users_id = [];
+      for (user_id in data.game.users) {
+        users_id.push(user_id);
+      }
+      const handle_singleGameUsers = Meteor.subscribe('singleGameUsers', users_id);
+      if (handle_singleGameUsers.ready()) {
+        data.users = Meteor.users//.find({}).fetch();
+      }
     }
-    const handle_singleGameUsers = Meteor.subscribe('singleGameUsers', this.props.game_id);
-    if (handle_singleGameUsers.ready()) {
-      console.log("--data.users reload--", Meteor.users.find().fetch());
-      data.users = Meteor.users.find().fetch();
-    }
+    // const handle_singleGameUsers = Meteor.subscribe('singleGameUsers', this.props.game_id);
+    // if (handle_singleGameUsers.ready()) {
+    //   console.log("--data.users reload--", Meteor.users.find().fetch());
+    //   data.users = Meteor.users.find().fetch();
+    // }
 
     console.log("getMeteorData - data : ", data);
     return data;
@@ -45,13 +55,15 @@ GameApp = React.createClass({
       return
     }
     let users = [];
-    console.log("rerender :",this.data.game.users);
-    // for(user_id in this.data.game.users) {
-    //   var user = this.data.users.findOne(user_id)
-    //   if (user) {
-    //     users.push(<li key={"user-" + user_id}>{user.username}</li>)
-    //   }
-    // }
+    console.log("rerender :", this.data.game.users);
+    console.log("users :", this.data.users);
+    for(user_id in this.data.game.users) {
+      var user = this.data.users.findOne({_id: user_id})
+      console.log(user_id, " -> user :",  user);
+      if (user) {
+        users.push(<li key={"user-" + user_id}>{user.username}</li>)
+      }
+    }
 
     return <ul>
       {users}
--- server/publications.js
+++ server/publications.js
@@ -5,16 +5,16 @@ Meteor.publish('games', () => {
 Meteor.publish('singleGame', _id => {
   return Games.find({_id});
 });
-Meteor.publish('singleGameUsers', _id => {
-  let game = Games.findOne({_id});
-  if (!game) {
-    return null;
-  }
-  let users_id = [];
-  for(user_id in game.users) {
-    users_id.push(user_id);
-  }
-  console.log("Publish is for ", this.user_id);
-  console.log("Publish game update : ", Meteor.users.find({_id:{'$in': users_id}}).fetch());
+Meteor.publish('singleGameUsers', users_id => {
+  // let game = Games.findOne({_id});
+  // if (!game) {
+  //   return null;
+  // }
+  // let users_id = [];
+  // for(user_id in game.users) {
+  //   users_id.push(user_id);
+  // }
+  // console.log("Publish is for ", this.user_id);
+  // console.log("Publish game update : ", Meteor.users.find({_id:{'$in': users_id}}).fetch());
   return Meteor.users.find({_id:{'$in': users_id}});
 });
